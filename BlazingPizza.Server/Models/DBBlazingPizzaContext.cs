8590d2b5c07b880f1253db7906ab9bc4194dc51c
DairoArenas28 <dairo.arenas802@pascualbravo.edu.co>
2024-05-27 17:59:01 -0500

    Agregar archivos de proyecto.

---
+++ BlazingPizza.Server/Models/DBBlazingPizzaContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Server.Models
+{
+    public class DBBlazingPizzaContext : DbContext
+    {
+        public DBBlazingPizzaContext(DbContextOptions<DBBlazingPizzaContext> options) : base(options)
+        {
+            
+        }
+
+        public virtual DbSet<Pizza> Pizza { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+    }
+}
